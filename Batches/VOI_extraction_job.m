e785daa0c31c102036362d039e6bb565d1e6b6b3
Matheus Boger <41137472+mathboger@users.noreply.github.com>
2024-07-29 19:36:20 +0200

    Add files via upload
    Started Batches folder

---
+++ Batches/VOI_extraction_job.m
@@ -0,0 +1,12 @@
+%-----------------------------------------------------------------------
+% Job saved on 25-Apr-2024 13:32:06 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.util.voi.spmmat = {'/data/mboger/Analysis/GLM/SPM.mat'};
+matlabbatch{1}.spm.util.voi.adjust = NaN;
+matlabbatch{1}.spm.util.voi.session = '<UNDEFINED>';
+matlabbatch{1}.spm.util.voi.name = 'striatum';
+matlabbatch{1}.spm.util.voi.roi{1}.mask.image = {'/data/mboger/Analysis/GLM/mask.nii,1'};
+matlabbatch{1}.spm.util.voi.roi{1}.mask.threshold = 0.5;
+matlabbatch{1}.spm.util.voi.expression = 'i1';
